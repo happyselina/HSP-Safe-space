ed01d286858dd44e174b92c782a5e2670d2c0f69
selina <selina.chen@thecarousell.com>
2025-09-21 14:28:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/HSP-Safe-space/',  // ⚡⚡⚡ 重點：必須是 /你的repo名稱/
+})
