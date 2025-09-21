4ab451e910e7027a12e8adf2abca2f8c73134454
selina <selina.chen@thecarousell.com>
2025-09-21 14:29:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HSP-Safe-space/',  // ⚡⚡⚡ 重點：必須是 /你的repo名稱/
+  base: '/HSP-Safe-space/', 
 })
